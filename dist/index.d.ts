409f4ed650cd05e0b788ec7691279361087eb31c
Aleksandar Stojanovic <sale4319@gmail.com>
2024-11-17 13:02:26 +0100

    feat: add storybook

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,37 @@
 import React from 'react';
 
-interface ButtonProps {
+interface ButtonProps$1 {
     label: string;
     action: () => void;
 }
-declare const Button: ({ label, action }: ButtonProps) => React.JSX.Element;
+declare const Button$1: ({ label, action }: ButtonProps$1) => React.JSX.Element;
 
-export { Button };
+interface ButtonProps {
+    /** Is this the principal call to action on the page? */
+    primary?: boolean;
+    /** What background color to use */
+    backgroundColor?: string;
+    /** How large should the button be? */
+    size?: "small" | "medium" | "large";
+    /** Button contents */
+    label: string;
+    /** Optional click handler */
+    onClick?: () => void;
+}
+/** Primary UI component for user interaction */
+declare function Button({ primary, size, backgroundColor, label, ...props }: ButtonProps): React.JSX.Element;
+
+type User = {
+    name: string;
+};
+interface HeaderProps {
+    user?: User;
+    onLogin?: () => void;
+    onLogout?: () => void;
+    onCreateAccount?: () => void;
+}
+declare const Header: ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => React.JSX.Element;
+
+declare const Page: React.FC;
+
+export { Button$1 as Button, Header, Page, Button as StoryButton };
---
+++ src/stories/index.ts
@@ -0,0 +1,3 @@
+export { default as StoryButton } from "./buttons";
+export { Header } from "./header";
+export { Page } from "./page";
