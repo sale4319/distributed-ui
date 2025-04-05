cec5e91c82c8cd57f32e7db410414ded5ef27eea
Aleksandar Stojanovic <sale4319@gmail.com>
2025-04-05 10:54:30 +0200

    fix: quiz default import

--- src/stories/forms/QuizForm/QuizForm.stories.tsx
+++ src/stories/forms/QuizForm/QuizForm.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Quiz } from "./QuizForm";
+import Quiz from "./QuizForm";
 
 const meta: Meta<typeof Quiz> = {
   title: "Forms/QuizForm",
